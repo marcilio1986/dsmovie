03890bc8b8aeab4f573453e0fc478a7400aaf046
Marcilio Bruno <marcilio.ti@gmail.com>
2022-03-12 23:53:41 -0300

    Find movies

---
+++ backend/src/main/java/com/devesuperior/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,7 @@
+package com.devesuperior.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;import com.devesuperior.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+}
